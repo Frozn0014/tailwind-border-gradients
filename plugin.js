667ea1717783412a02d1abcb9b08155da2d50a95
Frozn0014 <80000794+Frozn0014@users.noreply.github.com>
2023-05-24 10:57:34 -0400

    chore: format

--- plugin.js
+++ plugin.js
@@ -1,13 +1,13 @@
-const plugin = require("tailwindcss/plugin");
+const plugin = require('tailwindcss/plugin');
 
 const borderGradient = plugin(
   function ({ addUtilities, theme, e }) {
-    const values = theme("direction");
+    const values = theme('direction');
     const utilities = Object.entries(values).map(([key, value]) => {
       return {
         [`.${e(`border-gradient-to-${key}`)}`]: {
-          borderImage: `linear-gradient(${value}, var(--tw-gradient-stops)) 10`,
-        },
+          borderImage: `linear-gradient(${value}, var(--tw-gradient-stops)) 10`
+        }
       };
     });
 
@@ -16,16 +16,16 @@ const borderGradient = plugin(
   {
     theme: {
       direction: {
-        r: "to right",
-        l: "to left",
-        t: "to top",
-        b: "to bottom",
-        tr: "to top right",
-        tl: "to top left",
-        br: "to bottom right",
-        bl: "to bottom left",
-      },
-    },
+        r: 'to right',
+        l: 'to left',
+        t: 'to top',
+        b: 'to bottom',
+        tr: 'to top right',
+        tl: 'to top left',
+        br: 'to bottom right',
+        bl: 'to bottom left'
+      }
+    }
   }
 );
 
