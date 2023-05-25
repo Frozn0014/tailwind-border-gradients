c42ec68647494923f6e4bcf2c28a7a1bb9399e1d
Frozn0014 <80000794+Frozn0014@users.noreply.github.com>
2023-05-25 09:58:33 -0400

    feat: radial gradient support

--- plugin.js
+++ plugin.js
@@ -2,8 +2,8 @@ const plugin = require('tailwindcss/plugin');
 
 const borderGradient = plugin(
   function ({ addUtilities, theme, e }) {
-    const values = theme('direction');
-    const utilities = Object.entries(values).map(([key, value]) => {
+    const linearDirections = theme('linearDirections');
+    const linearUtilities = Object.entries(linearDirections).map(([key, value]) => {
       return {
         [`.${e(`border-gradient-to-${key}`)}`]: {
           borderImage: `linear-gradient(${value}, var(--tw-gradient-stops)) 10`
@@ -11,11 +11,20 @@ const borderGradient = plugin(
       };
     });
 
-    addUtilities(utilities);
+    const radialShapes = theme('radialShapes');
+    const radialUtilities = Object.entries(radialShapes).map(([key, value]) => {
+      return {
+        [`.${e(`border-radial-${key}`)}`]: {
+          borderImage: `radial-gradient(${value} at center, var(--tw-gradient-stops)) 10`
+        }
+      };
+    });
+
+    addUtilities([linearUtilities, radialUtilities]);
   },
   {
     theme: {
-      direction: {
+      linearDirections: {
         r: 'to right',
         l: 'to left',
         t: 'to top',
@@ -24,6 +33,10 @@ const borderGradient = plugin(
         tl: 'to top left',
         br: 'to bottom right',
         bl: 'to bottom left'
+      },
+      radialShapes: {
+        circle: 'circle',
+        ellipse: 'ellipse'
       }
     }
   }
